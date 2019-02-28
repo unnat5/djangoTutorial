1fb4a8f6a850f4bcdc2295144f5364ad047289ca
Unnat Singh <unnatsingh5@gmail.com>
2019-02-28 12:17:47 +0530

    Admin and Apps - Django Web Development with Python p.3

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Tutorial
+from tinymce.widgets import TinyMCE ## import TinyMCE for textfield
+from django.db import models  ## import models to modify textField.
+
+'''
+To access our site in admin page we need to register it here.
+'''
+
+## Customization admin page
+'''
+class TutorialAdmin(admin.ModelAdmin):
+	fields = ["tutorial_published","tutorial_title","tutorial_content",]
+	## changing the order of the fields.
+'''
+## we can cluster similar fields together
+class TutorialAdmin(admin.ModelAdmin):
+
+	fieldsets= [
+		("Title/date",{"fields":["tutorial_title","tutorial_published"]}),
+		("Content", {"fields":["tutorial_content"]}),
+	]
+
+	## modifying the textfield so have nice editor in it.
+	formfield_overrides = {
+		models.TextField: {'widget':TinyMCE()}
+
+	}
 
 # Register your models here.
+admin.site.register(Tutorial,TutorialAdmin)
+
+
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 '''
-Before making miigration we need to add the app in mysite/settings.py
+Before making migration we need to add the app in mysite/settings.py
 >> python manage.py makemigrations
 to see the actual sql code we could do this 
 >> python manage.py sqlmigrate main 0001
-and to implement that sql we do this s
+and to implement that sql we do this 
 >> python manage.py migrate
 '''
 class Tutorial(models.Model):
 	tutorial_title = models.CharField(max_length =200)
 	tutorial_content = models.TextField()
-	tutorial_published = models.DateTimeField("date published")
+	tutorial_published = models.DateTimeField("date published", default = timezone.now())
 
 	def __str__(self):
 		return self.tutorial_title
